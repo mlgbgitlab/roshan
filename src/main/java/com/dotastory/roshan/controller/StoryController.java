b30cac7f288d5fb224f158fc6110dc9c6abbee25
roshannnnn <dohia496@gmail.com>
2022-10-25 09:50:50 +0800

    init

---
+++ src/main/java/com/dotastory/roshan/controller/StoryController.java
@@ -0,0 +1,66 @@
+package com.dotastory.roshan.controller;
+
+import com.dotastory.roshan.utils.SSEUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@RequestMapping("/dotastory")
+public class StoryController {
+
+    @GetMapping("/connect")
+    public SseEmitter connect(HttpServletRequest request) {
+        String id =request.getRequestedSessionId();
+        return SSEUtils.connect(id);
+    }
+
+    @PostMapping("/send/story")
+    public void sendMessage(HttpServletRequest request,String story) {
+        String id = request.getRequestedSessionId();
+        SSEUtils.sendMessageByGiveNoticeAll(id, story);
+    }
+
+    private String getIpAddress(HttpServletRequest request) {
+        String ipAddress = null;
+        try {
+            ipAddress = request.getHeader("x-forwarded-for");
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getHeader("WL-Proxy-Client-IP");
+            }
+            if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+                ipAddress = request.getRemoteAddr();
+                if (ipAddress.equals("127.0.0.1")) {
+                    // 根据网卡取本机配置的IP
+                    InetAddress inet = null;
+                    try {
+                        inet = InetAddress.getLocalHost();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                    ipAddress = inet.getHostAddress();
+                }
+            }
+            // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+            if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
+                // = 15
+                if (ipAddress.indexOf(",") > 0) {
+                    ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
+                }
+            }
+        } catch (Exception e) {
+            ipAddress = "" ;
+        }
+        return ipAddress;
+
+    }
+}
---
+++ src/main/java/com/dotastory/roshan/utils/SSEUtils.java
@@ -0,0 +1,102 @@
+package com.dotastory.roshan.utils;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class SSEUtils {
+    private static Logger logger = LoggerFactory.getLogger(SSEUtils.class);
+    private static AtomicInteger ssecount = new AtomicInteger(0);
+    private static Map<String, SseEmitter> sseEmitterMap = Maps.newConcurrentMap();
+
+    public static SseEmitter connect(String userId) {
+        //设置超时时间，0表示不过期，默认是30秒，超过时间未完成会抛出异常
+        if(sseEmitterMap.containsKey(userId)){
+            return sseEmitterMap.get(userId);
+        }
+        SseEmitter sseEmitter = new SseEmitter(0L);
+        try {
+            sseEmitter.onCompletion(completionCallBack(userId));
+            sseEmitter.onTimeout(timeOutCallBack(userId));
+            sseEmitterMap.put(userId, sseEmitter);
+            ssecount.getAndIncrement();
+        } catch (Exception e) {
+            logger.info("USERID：{}——SSE ERROR:{}", userId, e);
+            removeUser(userId);
+        }
+
+        logger.info("创建链接:{}", userId);
+        return sseEmitter;
+    }
+
+    public static List<String> getIds() {
+        return Lists.newArrayList(sseEmitterMap.keySet());
+    }
+
+    public static int getUserCount() {
+        return ssecount.intValue();
+    }
+
+    private static Runnable completionCallBack(String userId) {
+        return () -> {
+            logger.info("结束连接,{}", userId);
+            removeUser(userId);
+        };
+    }
+
+    private static Runnable timeOutCallBack(String userId) {
+        return () -> {
+            logger.info("连接超时,{}", userId);
+            removeUser(userId);
+        };
+    }
+
+    public static void removeUser(String userId) {
+        sseEmitterMap.remove(userId);
+        //数量-1
+        ssecount.getAndDecrement();
+        logger.info("remove user id:{}", userId);
+    }
+
+    public static boolean sendMessage(String userId, String message) {
+        if (sseEmitterMap.containsKey(userId)) {
+            try {
+                sseEmitterMap.get(userId).send(message);
+            } catch (IOException e) {
+                logger.error("发给用户:{}, 消息发送失败:{}", userId, e.getMessage());
+                return false;
+            }
+        } else {
+            logger.error("用户{}关闭链接，进度同步终止", userId);
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean sendMessageByGiveNoticeAll(String userId, String message) {
+        if (sseEmitterMap.containsKey(userId)) {
+            try {
+               for(String key: sseEmitterMap.keySet()){
+                       sseEmitterMap.get(key).send(message);
+               }
+            } catch (IOException e) {
+                logger.error("用户:{}, 通知消息发送失败:{}", userId, e.getMessage());
+                return false;
+            }
+        } else {
+            logger.error("用户{}关闭链接，进度同步终止", userId);
+            return false;
+        }
+        return true;
+    }
+}
